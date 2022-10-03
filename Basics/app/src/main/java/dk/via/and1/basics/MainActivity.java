472bded4b7e2899e4e68e8d54089969396cce97a
Chris309283 <Christopher.holmelund@gmail.com>
2022-10-03 03:47:52 +0200

    Interaction

---
+++ Basics/app/src/main/java/dk/via/and1/basics/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.via.and1.basics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.margin_padding);
+    }
+}
